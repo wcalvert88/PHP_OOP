1271f6a2a5b970da7db40dbe51fdbeba86f3aafa
wcalvert88 <wcalvert88@gmail.com>
2018-04-18 19:20:19 -0400

    learned class inheritance

---
+++ class_inheritance.php
@@ -0,0 +1,23 @@
+<?php 
+
+class Cars {
+    var $wheels = 4;
+    function greeting() {
+        return "hello";
+    }
+    
+    function greeting2() {
+
+    }
+}
+
+$bmw = new Cars();
+
+class Trucks extends Cars {
+    var $wheels = 10;
+}
+
+$ford = new Trucks();
+
+echo $ford->wheels;
+?>
